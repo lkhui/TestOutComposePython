a657c83a3fe9394723033b4547a1cca573b1c24f
lkhui <lkhui2000@hotmail.com>
2023-03-30 11:42:02 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import json
+
+print("Hello")
+print("Hello")
+
+
+print(12311111)
+
+def fibo(num):
+    if num <= 1:
+      return 1
+    else:
+      return num * fibo(num-1)
+    
+print(fibo(-10))
+
